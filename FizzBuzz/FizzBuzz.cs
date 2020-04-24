9731f4e72766b87c9e77beff7c8cb222cca96882
Bamigbade, Emmanuel (LNG-LON) <BAMIGBEX@legal.regn.net>
2020-04-24 12:21:14 +0100

    fix range tests

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,7 @@ namespace FizzBuzz
     {
         public string Fizz(int v)
         {
-            return v == 101 ? new ArgumentException().ToString()
+            return v < 1 || v > 100 ? new ArgumentException().ToString()
                 : (v % 3 == 0) && (v % 5 == 0 ) ? "FizzBuzz" 
                 : v % 3 == 0 ? "Fizz"
                 : v % 5 == 0 ? "Buzz" : "";
