120452d8e3abcd8b80d0d2d082f287043231db6c
Bamigbade, Emmanuel (LNG-LON) <BAMIGBEX@legal.regn.net>
2020-04-24 12:05:37 +0100

    Pass test for multiples of 3and5

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,8 +6,10 @@ namespace FizzBuzz
     {
         public string Fizz(int v)
         {
-            return v % 3 == 0 ? "Fizz" : 
-                v % 5 == 0 ? "Buzz" : "";
+            return (v % 3 == 0) && (v % 5 == 0 ) ? "FizzBuzz" 
+                : v % 3 == 0 ? "Fizz"
+                : v % 5 == 0 ? "Buzz" : "";
         }
     }
 }
+ 
